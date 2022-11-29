eaf90a534da7fbf125dc37da47b39db64cad4ef8
danding <dandingkeji.top@gmail.com>
2022-11-29 23:57:20 +0800

    #挖矿

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,17 +1,46 @@
 
 const crypto = require("crypto");
+
+//创世区块
+const initBlock = {
+  index: 0,
+  data: 'hello nblockchain',
+  prevHash: '0',
+  timestamp: 1669737041347,
+  nonce: 135337,
+  hash: '00005440a1ea069e32569b623d682b423df89d60f7e0482abc036678d0592447'
+}
 class Blockchain{
   constructor(){
-    this.blockchain = []
+    this.blockchain = [initBlock]
     this.data = []
     this.difficulty = 4
-    const hash = this.computeHash(0,"0",Date.now(),"hello nblockchain",1)
-    console.log(hash)
   } 
 
+  //获取最新区块
+  getLatestBlock(){
+    return this.blockchain[this.blockchain.length-1]
+  }
   //挖矿
   mine(){
-
+    let nonce = 0 
+    const index = this.blockchain.length
+    const data = this.data
+    const prevHash = this.getLatestBlock().hash
+    const timestamp = Date.now()
+    let hash = this.computeHash(index,prevHash,timestamp,data,nonce)
+    while(hash.slice(0,this.difficulty) !== "0".repeat(this.difficulty)){
+      nonce++
+      hash = this.computeHash(index,prevHash,timestamp,data,nonce)
+    }
+    console.log("挖矿结束",{
+      index,
+      data,
+      prevHash,
+      timestamp,
+      nonce,
+      hash
+    })
   }
 
   //生成新区块
@@ -31,3 +60,4 @@ class Blockchain{
 }
 
 let blockchain = new Blockchain()
+blockchain.mine()
