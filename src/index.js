a9a2d9792b4cbf8d22f9c46af13315e46b48428f
danding <dandingkeji.top@gmail.com>
2022-11-30 23:51:09 +0800

    #格式化输出

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 const vorpal = require('vorpal')();
+const Table = require('cli-table');
 const BlockChain = require('./blockchain');
 const blockchain = new BlockChain();
+
+function formatLog(data){
+  if(!Array.isArray(data)){
+    data = [data];
+  }
+  const first = data[0];
+  const head = Object.keys(first);
+  const table = new Table({
+    head: head,
+    colWidths: [10, 20, 40, 20, 10, 40],
+  });
+  const res = data.map(v => {
+    return head.map(h => v[h]);
+  })
+  table.push(...res);
+  console.log(table.toString());
+}
+
+
+
 vorpal.command('mine', '挖矿').action(function (args, callback) {
 	const newBlock = blockchain.mine();
 	if (newBlock) {
-		this.log(newBlock);
+		formatLog(newBlock);
 	}
 	callback();
 });
 vorpal.command('chain', '查看区块链').action(function (args, callback) {
-	this.log(blockchain.blockchain);
+	formatLog(blockchain.blockchain);
 	callback();
 });
 // vorpal
